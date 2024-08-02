27e3ccbc2425f063c5692047c09c6babd01c2aab
Alex <lesha.rudiko@gmail.com>
2024-08-02 12:03:46 +0300

    fix: bag cookie and searc product

--- src/shared/useCoockieLogin.ts
+++ src/shared/useCoockieLogin.ts
@@ -3,15 +3,15 @@ import {axiosClient} from "@/axiosClient";
 import store from "@/store";
 
 export function useCoockieLogin(){
-    const regExp = /token\S[\S]+/
+    const regExp = /token=[A-Z0-9]\S+/gi
     const coockieStore = document.cookie.split(';')
     const matchToken = coockieStore.toString().match(regExp)
+    const searchStr = matchToken ? matchToken[0] : null
 
-    if(matchToken){
-        const bytes = CryptoJS.AES.decrypt(matchToken[0].slice(6), new Date().getDay() + 'Hurst')
+    if(searchStr?.split('=')[1] !== 'undefined'){
+        const bytes = CryptoJS.AES.decrypt(searchStr!.split('=')[1], new Date().getDay() + 'Hurst')
         const decode = bytes.toString(CryptoJS.enc.Utf8)
         const keys = decode.split(':')
-
 
         axiosClient.get(`/users?email=${keys[0]}`).then((res) => {
             store.dispatch('setUser', res.data[0])
